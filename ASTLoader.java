3345b3475ca5fbd7caa24e20d8a846b590187b18
lxy <852559563@qq.com>
2022-06-03 10:24:17 +0800

    1.1 finish

--- ASTLoader.java
+++ ASTLoader.java
@@ -5,198 +5,62 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
-class ModuleNode {
-    String ID;
-    ArrayList<DefinitionNode> definitionNodes = new ArrayList<>();
+class Member {
+    String type;
 
-    public ModuleNode(String ID) {
-        this.ID = ID;
-    }
-}
+    LinkedHashMap<String, String> declarators;
 
-class DefinitionNode {
-    StructType structType;
-    String struct_ID;
-    Module module;
-
-    public DefinitionNode(Module module) {
-        this.module = module;
-    }
-
-    public DefinitionNode(String struct_ID) {
-        this.struct_ID = struct_ID;
-    }
-
-    public DefinitionNode(StructType structType) {
-        this.structType = structType;
-    }
-}
-
-enum Base_type_spec {CHAR, STRING, BOOLEAN, FLOAT, DOUBLE, LONGDOUBLE, SHORT, INT16, LONG, INT32, LONGLONG, INT64, INT8, USHORT, UINT16, ULONG, UINT32, ULONGLONG, UINT64, UINT8}
-
-class ScopeName {
-    boolean isFirstExist;
-    ArrayList<String> id_list;
-
-    public ScopeName(boolean isFirstExist, ArrayList<String> id_list) {
-        this.isFirstExist = isFirstExist;
-        this.id_list = id_list;
+    public Member(String type) {
+        this.type = type;
+        this.declarators = new LinkedHashMap<>();
     }
 
     @Override
     public String toString() {
-        StringBuilder stringBuilder = new StringBuilder();
-        if (isFirstExist) stringBuilder.append("::");
-        for (int i = 0; i < id_list.size(); i++) {
-            stringBuilder.append(id_list.get(i));
-            if (i != id_list.size() - 1) stringBuilder.append("::");
+        StringBuilder sb = new StringBuilder();
+        sb.append(type).append("   :\n");
+        for (var entry : declarators.entrySet()) {
+            sb.append(entry.getKey()).append(" ").append(entry.getValue()).append("\n");
         }
-        return stringBuilder.toString();
+        return sb.toString();
     }
 }
 
-class StructType {
-    String ID;
-    ArrayList<Member> members = new ArrayList<Member>();
-}
-
-class Member {
-    Base_type_spec base_type_spec;
-    ScopeName scopeName;
-    StructType struct;
-
-
-    DecNode decNode;
-}
-
-class DecNode {
-    String name;
-
-    SimpleExpNode array;
-
-    enum Dec_type {Array, Simple, None}
-
-    Dec_type type;
-
-    ExpListNode expListNode;
-    SimpleExpNode simpleExpNode;
-
-    public DecNode(String name, SimpleExpNode array, Dec_type type, ExpListNode expListNode, SimpleExpNode simpleExpNode) {
-        this.name = name;
-        this.array = array;
-        this.type = type;
-        this.expListNode = expListNode;
-        this.simpleExpNode = simpleExpNode;
-    }
-}
-
-class ExpListNode {
-    ArrayList<SimpleExpNode> simpleExpNodes = new ArrayList<>();
-}
-
-class SimpleExpNode {
-    enum Exp_Type {
-        OR("|"),
-        XOR("^"),
-        AND("&"),
-        SHIFTR("<<"),
-        SHIFTL(">>"),
-        ADD("+"),
-        MULTMUL("*"),
-        MULTDIV("/"),
-        MULTREM("%"),
-        ;
-
-        private String name;
-
-        Exp_Type(String name) {
-        }
-
-        @Override
-        public String toString() {
-            return this.name;
-        }
-    }
-
-
-    public Exp_Type exp_type;
-    public ArrayList<UnaryExpr> exprs = new ArrayList<>();
-
-    public SimpleExpNode(Exp_Type exp_type) {
-        this.exp_type = exp_type;
-    }
-}
-
-class UnaryExpr {
-    enum SIGN {
-        MINUS("-"),
-        PLUS("+"),
-        OPP("~"),
-        EMPTY("");
-        private String name;
-
-        SIGN(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public String toString() {
-            return this.name;
-        }
-    }
-
-
-    SIGN sign;
-    String literal;
-
-    public UnaryExpr(SIGN sign, String literal) {
-        this.sign = sign;
-        this.literal = literal;
-    }
-}
-
-class Literal {
-    enum DATA_TYPE {INTEGER, FLOATING_PT, CHAR, STRING, BOOLEAN}
-
-    private DATA_TYPE data_type;
-    private String value;
-
-    public Literal(DATA_TYPE data_type, String value) {
-        this.data_type = data_type;
-        this.value = value;
-    }
-}
 
 class TreeNode {
     public ArrayList<TreeNode> children = new ArrayList<>();
 
     enum NODETYPE {
-        DEFINITION,
         MODULE,
-        STRUCT,
-        DECLARATION,
-        TYPESPEC
+        STRUCT
     }
 
     public NODETYPE nodetype;
     //module
     public String module_id;
-    public ArrayList<DefinitionNode> definitionNodes = new ArrayList<>();
+    public ArrayList<TreeNode> definitions = new ArrayList<>();
 
     //struct
     public String struct_id;
     public ArrayList<Member> members = new ArrayList<>();//can be null
 
-
-    //declaration
-    public String dec_type;
-//    public ArrayList<Declaration> declarations = new ArrayList<>();
-
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        switch (nodetype) {
+            case MODULE:
+                sb.append("MODULE " + module_id + " :\n");
+                for (var def : definitions) sb.append(def);
+                break;
+            case STRUCT:
+                sb.append("STRUCT " + struct_id + " :\n");
+                for (var mem : members) sb.append(mem.toString());
+                break;
+        }
+        return sb.toString();
+    }
 }
 
 class ASTLoader extends MIDLBaseVisitor<TreeNode> {
@@ -209,40 +73,90 @@ class ASTLoader extends MIDLBaseVisitor<TreeNode> {
         ParseTree tree = parser.specification();
         ASTLoader astLoader = new ASTLoader();
         astLoader.visit(tree);
-        for (var i : astLoader.symbol_stack) System.out.println(i);
-//        for (var i : astLoader.exp_stack) System.out.print(i);
+        //  for (var i : astLoader.symbol_stack) System.out.println(i);
+        //  while (!astLoader.exp_queue.isEmpty()) System.out.print(astLoader.exp_queue.poll());
     }
 
     TreeNode root = new TreeNode();
+    String simple_exp = "";
+    LinkedHashMap<String, String> declarator_map = new LinkedHashMap<>();
     Stack<String> symbol_stack = new Stack<>();
     Queue<String> exp_queue = new LinkedList<>();//start from or_expr
 
+
+    //semantic analysis
+    ArrayList<String> struct_names = new ArrayList<>();
+
+    /**
+     * specification: (definition)+;
+     *
+     * @param ctx
+     * @return
+     */
     @Override
     public TreeNode visitSpecification(MIDLParser.SpecificationContext ctx) {
-        return super.visitSpecification(ctx);
+        ArrayList<TreeNode> specification = new ArrayList<>();
+        for (int i = 0; i < ctx.getChildCount(); i++) {
+            specification.add(visit(ctx.getChild(i)));
+        }
+        for (var spec : specification) System.out.println(spec);
+        return null;
     }
 
+    /**
+     * definition: type_decl ';' | module ';';
+     *
+     * @param ctx
+     * @return
+     */
     @Override
     public TreeNode visitDefinition(MIDLParser.DefinitionContext ctx) {
-        return super.visitDefinition(ctx);
+        return visit(ctx.getChild(0));
     }
 
+    /**
+     * module: 'module' ID '{' definition+ '}';
+     *
+     * @param ctx
+     * @return
+     */
     @Override
     public TreeNode visitModule(MIDLParser.ModuleContext ctx) {
-        return super.visitModule(ctx);
+        TreeNode treeNode = new TreeNode();
+        treeNode.nodetype = TreeNode.NODETYPE.MODULE;
+        treeNode.module_id = ctx.ID().getText();
+        for (int i = 0; i < ctx.getChildCount() - 4; i++) {
+            treeNode.definitions.add(visit(ctx.definition(i)));
+        }
+        return treeNode;
     }
 
     @Override
     public TreeNode visitType_decl(MIDLParser.Type_declContext ctx) {
-        return super.visitType_decl(ctx);
+        TreeNode treeNode = new TreeNode();
+        if (ctx.getChildCount() > 1) {
+            treeNode.struct_id = ctx.ID().getText();
+            treeNode.nodetype = TreeNode.NODETYPE.STRUCT;
+        } else {
+            return visit(ctx.struct_type());
+        }
+
+        return treeNode;
     }
 
+    /**
+     * struct_type: 'struct' ID '{'   member_list '}';
+     *
+     * @param ctx
+     * @return
+     */
     @Override
     public TreeNode visitStruct_type(MIDLParser.Struct_typeContext ctx) {
         TreeNode treeNode = new TreeNode();
+        treeNode.struct_id = ctx.ID().getText();
         treeNode.nodetype = TreeNode.NODETYPE.STRUCT;
-        visit(ctx.getChild(3));
-//        treeNode.members = visit(ctx.getChild(3)).members;
+        treeNode.members = visit(ctx.getChild(3)).members;
+        struct_names.clear();
         return treeNode;
     }
 
@@ -258,8 +172,15 @@ class ASTLoader extends MIDLBaseVisitor<TreeNode> {
         for (int i = 0; i < ctx.getChildCount() / 3; i++) {
             visit(ctx.getChild(3 * i));//type_spec
             visit(ctx.getChild(3 * i + 1));//declaration
+            String type = symbol_stack.pop();
+            Member m = new Member(type);
+            m.declarators.putAll(declarator_map);
+            declarator_map.get(0);
+            treeNode.members.add(m);
+            declarator_map.clear();
+
         }
-        return null;
+        return treeNode;
     }
 
     /**
@@ -271,6 +192,9 @@ class ASTLoader extends MIDLBaseVisitor<TreeNode> {
      */
     @Override
     public TreeNode visitType_spec(MIDLParser.Type_specContext ctx) {
+        if (ctx.getChild(0).getChild(0).getText().equals("struct")) {
+            symbol_stack.push(ctx.getChild(0).getText().substring(0, ctx.getChild(0).getText().indexOf("{")));
+        }
         return super.visitType_spec(ctx);
     }
 
@@ -287,7 +211,7 @@ class ASTLoader extends MIDLBaseVisitor<TreeNode> {
             dtype += ctx.getChild(i).getText();
         }
         symbol_stack.push(dtype);
-        return super.visitScoped_name(ctx);
+        return null;
     }
 
     /**
@@ -304,13 +228,12 @@ class ASTLoader extends MIDLBaseVisitor<TreeNode> {
         } else {
             visit(ctx.getChild(0));
         }
-        return super.visitBase_type_spec(ctx);
+        return null;
     }
 
     @Override
     public TreeNode visitFloating_pt_type(MIDLParser.Floating_pt_typeContext ctx) {
-        String dtype = ctx.getChild(0).getText();
-        symbol_stack.push(dtype);
+        symbol_stack.push(ctx.getChild(0).getText());
         return null;
     }
 
@@ -347,12 +270,12 @@ class ASTLoader extends MIDLBaseVisitor<TreeNode> {
      */
     @Override
     public TreeNode visitDeclarators(MIDLParser.DeclaratorsContext ctx) {
-        TreeNode treeNode = new TreeNode();
-      //  treeNode.dec_type = symbol_stack.pop();
-        for (int i = 1; i < ctx.getChildCount() - 1; i = i + 2) {
-//            out.print(ctx.getChild(i) + " ");
-            visit(ctx.getChild(i + 1));
+        for (int i = 0; i < (ctx.getChildCount() + 1) / 2; i++) {
+            visit(ctx.declarator(i));
+
         }
+
+
         return null;
     }
 
@@ -369,58 +292,102 @@ class ASTLoader extends MIDLBaseVisitor<TreeNode> {
 
     /**
      * simple_declarator : ID ('=' or_expr)?;
+     *
      * @param ctx
      * @return
      */
     @Override
     public TreeNode visitSimple_declarator(MIDLParser.Simple_declaratorContext ctx) {
-        return super.visitSimple_declarator(ctx);
+        String name = ctx.ID().getText();
+        if (ctx.getChildCount() > 1) visit(ctx.or_expr());
+
+        //todo: 2.1 检查同一个struct下是否有同名变量
+        // 在检查declarator的时候,检查一下当前的name是否和已经有的map冲突
+        if (struct_names.contains(name)) {
+            System.err.println(
+                    "line " + ctx.start.getLine() + ":" + ctx.start.getCharPositionInLine() + " repeated variable '" + ctx.start.getText() + "'" +
+                            "    semantic error 1.1: " +
+                            "In the same struct space, there cannot be variables with the same name.  "
+            );
+            throw new RuntimeException("Semantic Error 1.1");
+        }
+        StringBuilder exp = new StringBuilder();
+        while (!exp_queue.isEmpty()) exp.append(exp_queue.poll()).append(" ");
+        declarator_map.put(name, exp.toString());
+        struct_names.add(name);
+        return null;
     }
 
     /**
      * array_declarator : ID '[' or_expr ']' ('=' exp_list )?;
+     *
      * @param ctx
      * @return
      */
     @Override
     public TreeNode visitArray_declarator(MIDLParser.Array_declaratorContext ctx) {
-        String name=ctx.ID().getText();
+        String name = ctx.ID().getText();
         visit(ctx.or_expr());
-        StringBuilder or_expr= new StringBuilder();
+        StringBuilder or_expr = new StringBuilder();
         while (!exp_queue.isEmpty()) or_expr.append(exp_queue.poll());
-        name+="["+or_expr.toString()+"]";
+        //处理数组大小方案,放进type里面
+        symbol_stack.push(symbol_stack.pop() + "[" + or_expr + "]");
+
         visit(ctx.exp_list());
+
+        if (struct_names.contains(name)) {
+            System.err.println(
+                    "line " + ctx.start.getLine() + ":" + ctx.start.getCharPositionInLine() + " repeated variable '" + ctx.start.getText() + "'" +
+                            "    semantic error 1.1: " +
+                            "In the same struct space, there cannot be variables with the same name.  "
+            );
+            throw new RuntimeException("Semantic Error 1.1");
+        }
+        //hashmap
+        StringBuilder exp = new StringBuilder();
+        while (!exp_queue.isEmpty()) exp.append(exp_queue.poll()).append(" ");
+        declarator_map.put(name, exp.toString());
+        struct_names.add(name);
         return null;
     }
 
     /**
      * exp_list : '[' or_expr ( ',' or_expr )* ']';
+     *
      * @param ctx
      * @return
      */
     @Override
     public TreeNode visitExp_list(MIDLParser.Exp_listContext ctx) {
-        ArrayList<String> exp_list=new ArrayList<>();
+//        ArrayList<String> exp_list = new ArrayList<>();
         int n = (ctx.getChildCount() - 1) / 2;
         for (int i = 0; i < n; i++) {
             visit(ctx.getChild(2 * i + 1));
-            String exp="";
-            while (!exp_queue.isEmpty()) exp+=exp_queue.poll();
-            exp_list.add(exp);
+//            String exp = "";
+//            while (!exp_queue.isEmpty()) exp += exp_queue.poll();
+//            exp_list.add(exp);
         }
 
-        for (var i:exp_list) System.out.println(i);//todo:得到explist 之后干嘛呢
+//        for (var i : exp_list) System.out.println(i);//todo:得到explist 之后干嘛呢
         return null;
     }
 
+    /**
+     * or_expr : xor_expr ('|' xor_expr )*;
+     *
+     * @param ctx
+     * @return
+     */
     @Override
     public TreeNode visitOr_expr(MIDLParser.Or_exprContext ctx) {
         visit(ctx.xor_expr(0));
         int n = (ctx.getChildCount() - 1) / 2;
         for (int i = 0; i < n; i++) {
-            exp_queue.offer(ctx.getChild(2 * i + 1).getText());
+            simple_exp += ctx.getChild(2 * i + 1).getText();
             visit(ctx.xor_expr(i + 1));
         }
+        exp_queue.offer(simple_exp);
+        simple_exp = "";
         return null;
     }
 
@@ -429,7 +396,7 @@ class ASTLoader extends MIDLBaseVisitor<TreeNode> {
         visit(ctx.and_expr(0));
         int n = (ctx.getChildCount() - 1) / 2;
         for (int i = 0; i < n; i++) {
-            exp_queue.offer(ctx.getChild(2 * i + 1).getText());
+            simple_exp += ctx.getChild(2 * i + 1).getText();
             visit(ctx.and_expr(i + 1));
         }
         return null;
@@ -440,7 +407,7 @@ class ASTLoader extends MIDLBaseVisitor<TreeNode> {
         visit(ctx.shift_expr(0));
         int n = (ctx.getChildCount() - 1) / 2;
         for (int i = 0; i < n; i++) {
-            exp_queue.offer(ctx.getChild(2 * i + 1).getText());
+            simple_exp += ctx.getChild(2 * i + 1).getText();
             visit(ctx.shift_expr(i + 1));
         }
         return null;
@@ -451,7 +418,7 @@ class ASTLoader extends MIDLBaseVisitor<TreeNode> {
         visit(ctx.add_expr(0));
         int n = (ctx.getChildCount() - 1) / 2;
         for (int i = 0; i < n; i++) {
-            exp_queue.offer(ctx.getChild(2 * i + 1).getText());
+            simple_exp += ctx.getChild(2 * i + 1).getText();
             visit(ctx.add_expr(i + 1));
         }
         return null;
@@ -462,8 +429,7 @@ class ASTLoader extends MIDLBaseVisitor<TreeNode> {
         visit(ctx.mult_expr(0));
         int n = (ctx.getChildCount() - 1) / 2;
         for (int i = 0; i < n; i++) {
-            exp_queue.offer(ctx.getChild(2 * i + 1).getText());
-
+            simple_exp += ctx.getChild(2 * i + 1).getText();
             visit(ctx.mult_expr(i + 1));
         }
         return null;
@@ -474,7 +440,7 @@ class ASTLoader extends MIDLBaseVisitor<TreeNode> {
         visit(ctx.unary_expr(0));
         int n = (ctx.getChildCount() - 1) / 2;
         for (int i = 0; i < n; i++) {
-            exp_queue.offer(ctx.getChild(2 * i + 1).getText());
+            simple_exp += ctx.getChild(2 * i + 1).getText();
             visit(ctx.unary_expr(i + 1));
         }
         return null;
@@ -483,7 +449,7 @@ class ASTLoader extends MIDLBaseVisitor<TreeNode> {
     @Override
     public TreeNode visitUnary_expr(MIDLParser.Unary_exprContext ctx) {
         if (ctx.getChildCount() != 1) {
-            exp_queue.offer(ctx.getChild(0).getText());
+            simple_exp += ctx.getChild(0).getText();
         }
         visit(ctx.literal());
         return null;
@@ -491,7 +457,7 @@ class ASTLoader extends MIDLBaseVisitor<TreeNode> {
 
     @Override
     public TreeNode visitLiteral(MIDLParser.LiteralContext ctx) {
-        exp_queue.offer(ctx.getChild(0).getText());
+        simple_exp += ctx.getChild(0).getText();
         return null;
     }
 }
